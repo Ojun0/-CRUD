ddc7f7ced0c80257cd7132064a39489b6817766e
Ojun0 <ojy814@naver.com>
2022-06-12 12:36:36 +0900

    회원 가입 메인 메소드 추가

---
+++ CRUD project/src/singup/Main.java
@@ -0,0 +1,55 @@
+package singup;
+
+import java.util.*;
+
+public class Main {
+
+		public static void main(String[] args) {
+			ArrayList<user> users = new ArrayList<user>();
+			Scanner sc = new Scanner(System.in);
+			int n = 0;
+			
+			Boolean flag = true;
+			while (flag) {
+				System.out.println("-------------------------------------------------");
+				System.out.println("1. 회원가입 2.회원탈퇴 3.회원리스트 4.회원 정보 수정 5.종료");
+				System.out.println("-------------------------------------------------");
+				System.out.println("실행하실 메뉴의 번호를 입력해주세요:");
+				n= sc.nextInt();
+				
+				switch (n) {
+					case 1: //회원가입
+						System.out.println();
+						System.out.println("회원의 정보를 입력해주세요");
+						System.out.println("--------------------");
+						System.out.println("이름:");
+						String name =sc.next();
+						System.out.println("아이디:");
+						String id = sc.next();
+						System.out.println("비밀번호 :");
+						String pw = sc.next();
+						users.add(new user(name, id, pw));
+						System.out.println("회원가입이 되었습니다");
+						break;
+					
+					case 2: //회원탈퇴	
+						break;
+					
+					case 3: //회원리스트
+						break;
+					
+					case 4: //회원 정보 수정
+						break;
+						
+					case 5: //종료
+						flag = false;
+						System.out.println("시스템이 종료되었습니다.");
+						break;
+					
+					default:
+						System.out.println("1~5 사이에 숫자만 입력해주세요.");
+						break;
+				}
+			}
+		}
+}
