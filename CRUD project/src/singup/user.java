1587c85341bd3c5dd4b26fcd188428898292dd27
JiHyeok47 <chlwlgur0407@naver.com>
2022-06-12 18:59:31 +0900

    회원 이메일, 나이 정보 추가

--- CRUD project/src/singup/user.java
+++ CRUD project/src/singup/user.java
@@ -4,13 +4,15 @@ public class user {
 	String name;
 	String id;
 	String pw;
+	String email;
+	String age;
 	
-	
-	public user(String name, String id, String pw) {
+	public user(String name, String id, String pw, String email, String age) {
 		this.name = name;
 		this.id = id;
 		this.pw = pw;
-		
+		this.email = email;
+		this.age = age;
 	}
 	
 	public String getName() {
@@ -38,7 +40,23 @@ public class user {
 		
 	}
 	
+	public String getEmail() {
+		this.email = email;
+	}
+	
+	public void setEmail(String email) {
+		return email;
+	}
+	
+	public String getAge() {
+		this.age = age;
+	}
+	
+	public void setAge(String age) {
+		return age;
+	}
+	
 	public String toString( ) {
-		return "----------- \n 회원 이름:" + name  + "\n 아이디 :" + id + "\n 비밀번호" + pw + "\n ------------";
+		return "----------- \n 회원 이름:" + name  + "\n 아이디 :" + id + "\n 비밀번호" + pw + " 이메일:" + email + "나이:" + age +"\n ------------";
 	}
 }
